bf18f6527aa6ad2a8c193c445f772c327e8e41e1
sero1910 <pcz.pracownik1@outlook.com>
2025-11-21 21:05:16 +0100

    Stworzenie funkcji: wyswietl_menu, pobierz dane studenta

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 from models import Student
 
-student1 = Student("Wojciech","Serodzinski","Niepodległości 7d", "48799","9128123","Mężczyzna")
-print(student1)
+def wyswietl_menu():
+    print("Menu:")
+    print("1. Dodaj studenta")
+    print("2. Wyświetl wszystkich studentów")
+    print("3. Wyszukaj studenta")
+    print("4. Usuń studenta")
+    print("0. Wyjście")
+    print("==========================")
+
+def pobierz_dane_studenta():
+     print("\n--- Dodawanie nowego studenta ---")
+     imie = input("Podaj imię: ")
+     nazwisko = input("Podaj nazwisko: ")
+     adres = input("Podaj adres: ")
+     nr_indeksu = input("Podaj numer indeksu: ")
+     pesel = input("Podaj PESEL: ")
+     plec = input("Podaj płeć (M/K): ").upper()
+
+     return Student(imie, nazwisko, adres, nr_indeksu, pesel, plec)
+
+
+
+
+
+def main():
+    
+
+    
+        wyswietl_menu()
+
+
+if __name__ == "__main__":
+    main()
